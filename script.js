156e93766b36dd09fa0f89d6eafca8b6f6e6ae74
Gabriel Soares <115032490+gasosi@users.noreply.github.com>
2025-07-02 16:16:07 -0300

    d

--- script.js
+++ script.js
@@ -10,3 +10,6 @@ let linguagens = ["JavaScript", "Phyton", "C"];
 console.log (linguagens [0]) //javascript
 console.log (linguagens[1]) //phyton
 console.log (linguagens [2]) // C
+
+const lista = document.querySelector ("#lista");
+lista.textContent =  linguagens [0];
