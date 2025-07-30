0a9e9cc23bc2959b001c872fbc54b1528031d03f
Gabriel Soares <115032490+gasosi@users.noreply.github.com>
2025-07-30 16:16:42 -0300

    sdfsdf

--- script.js
+++ script.js
@@ -15,3 +15,22 @@ const lista = document.querySelector ("#lista");
 lista.textContent =  linguagens [2];
 lista.textContent =  linguagens [1];
 lista.textContent =  linguagens [0];
+
+console.log(aluno1.nome); 
+console.log(aluno1.idade); 
+console.log(aluno1.anoLetivo);
+
+console.log(aluno1.materiasFavoritas[1]);
+
+
+
+let aluno = {
+ nome: "Gabriel",
+  idade: 13,
+  anoLetivo: "2ºsérie",
+  materiasFavoritas: ["Geografia","Programação","Arte"]
+};
+  
+
+const objeto = document.querySelector("#aluno");
+objeto.textContent = aluno1.nome;
